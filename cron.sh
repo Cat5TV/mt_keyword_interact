b0cb501919f9adc82483d13abe95c607a0d83c69
Robbie Ferguson <contact@category5.tv>
2016-06-28 08:50:15 -0400

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# Call this script only when Minetest is not running (eg., just before running your daily backup).
+# Call this script via cron to automatically change your interact keyword
+# The minimum time between update should be reasonable (eg., don't update more than once per hour)
+# Keep in mind this also updates tps_signs with the new keyword
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 shuf -n 1 $DIR/keywords.txt > $DIR/keyword.txt

930fc3708f7e2a01eab251e7d9f9fe54a1c7f808
Robbie Ferguson <contact@category5.tv>
2016-06-28 09:31:41 -0400

    Add random option
    DonBatman's idea: add random string output as an option. Now, supports command line argument random or keyword.

--- cron.sh
+++ cron.sh
@@ -3,4 +3,16 @@
 # The minimum time between update should be reasonable (eg., don't update more than once per hour)
 # Keep in mind this also updates tps_signs with the new keyword
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-shuf -n 1 $DIR/keywords.txt > $DIR/keyword.txt
+
+if [[ $1 == 'random' ]]
+then
+  rand=$(strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 4 | tr '[:upper:]' '[:lower:]' | tr -d '\n'; echo)
+  echo $rand > $DIR/keyword.txt
+elif [[ $1 == 'keyword' ]]
+then
+  shuf -n 1 $DIR/keywords.txt > $DIR/keyword.txt
+else
+  echo "Usage: $0 type"
+  echo '  Where "type" is either random or keyword'
+  echo
+fi
